ecfa9ce79af7dee4d250b579f9a339632f22d3ea
lsalvatierra <luisangel.salvat@gmail.com>
2024-04-13 21:53:33 -0500

    product funciones javascript v2

--- src/main/resources/static/js/web/backoffice/product/formproduct.js
+++ src/main/resources/static/js/web/backoffice/product/formproduct.js
@@ -1,3 +1,13 @@
 $(document).on("click", "#btnagregar", function(){
+    $("#txtnomproduct").val("");
+    $("#txtunitpriceproduct").val("");
+    $("#hddprodcod").val("0");
+    $("#modalproduct").modal("show");
+})
+
+$(document).on("click", ".btnactualizar", function(){
+    $("#txtnomproduct").val($(this).attr("data-prodname"));
+    $("#txtunipriceproduct").val($(this).attr("data-produnit"));
+    $("#hddprodcod").val($(this).attr("data-prodcod"));
     $("#modalproduct").modal("show");
 })
